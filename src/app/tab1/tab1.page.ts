1ca986db57347a50a479a0516f03a1a78145e821
Brooks Forsyth <brooks@brookss-air.lan>
2021-02-07 12:10:46 -0500

    init commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from "@capacitor/core"
+import { CameraPreviewOptions, CameraPreviewPictureOptions } from '@capacitor-community/camera-preview';
+import '@capacitor-community/camera-preview'
+const { CameraPreview } = Plugins;
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+  async ngOnInit() {
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'rear',
+      parent: 'cameraPreview',
+      className: 'cameraPreview'
+    };
+    CameraPreview.start(cameraPreviewOptions);
+  }
 
 }
